e41688115c50ff356b0cc2c21ee0c6680df3fdb0
Ronnie Mayberry <ronnielmayberry@gmail.com>
2016-05-20 09:10:47 -0500

    Add files via upload

---
+++ Lesson--6/Receive_and_Return.py
@@ -0,0 +1,34 @@
+'''
+Created on November 19, 2014
+
+@author: mayberryr
+'''
+# Receive and Return
+# Demonstrates parameters and return values.
+# Ronnie Mayberry
+# Wednesday, November 19, 2014
+
+def display(message):
+    print(message)
+
+def give_me_five():
+    five = 5
+    return five
+
+def ask_yes_no(question):
+    """Ask a yes or no question."""
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+    return response
+
+# main
+display("Here' s a message for you.\n")
+
+number = give_me_five()
+print("Here's what I got from give_me_five():", number)
+
+answer = ask_yes_no("\nPlease enter 'y' or 'n': ")
+print("Thanks for entering: ", answer)
+
+input("\n\nPress the enter key to exit.")
