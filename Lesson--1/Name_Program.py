0cfb422fcf552f906930675674fa233f54b202f3
Ronnie Mayberry <ronnielmayberry@gmail.com>
2016-05-20 09:08:12 -0500

    Add files via upload

---
+++ Lesson--1/Name_Program.py
@@ -0,0 +1,10 @@
+'''
+Created on September 18, 2014
+
+@author: mayberryr
+'''
+print ("Ronnie Mayberry")
+input ("\n\nPress the enter key to exit.")
+#Ronnie Mayberry
+#Thursday, September 18, 2014
+#The purpose of the program is to print out my name and then wait for input from the user to close.
---
+++ Lesson--1/Test_Program.py
@@ -0,0 +1,12 @@
+'''
+Created on September 18, 2014
+
+@author: mayberryr
+'''
+print("Game Over")
+print("You lose.")
+print("Bye.")
+#My name is Ronnie Mayberry.
+#It is Thursday, September 18, 2014.
+#Comments are used to make notes for the editor of the code.
+input("\n\nPress the enter key to exit.")
