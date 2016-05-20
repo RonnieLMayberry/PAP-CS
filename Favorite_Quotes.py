3f43d2da617560250cb3c3d7f6095fcb7fd24703
Ronnie Mayberry <ronnielmayberry@gmail.com>
2016-05-20 09:07:39 -0500

    Add files via upload

---
+++ Favorite_Quotes.py
@@ -0,0 +1,11 @@
+'''
+Created on September 19, 2014
+
+@author: mayberryr
+'''
+print ("Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. - Thomas A. Edison")
+print ("Keep your eyes on the stars, and your feet on the ground. - Theodore Roosevelt")
+print ("The secret of getting ahead is getting started. - Mark Twain")
+#Ronnie Mayberry
+#Friday, September 19, 2014
+#The purpose of this program is to print out my three favorite quotes when run.
