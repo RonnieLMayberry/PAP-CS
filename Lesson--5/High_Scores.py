bb5c3f3299e4f2be0592a150e5d936f3e4e0cc9c
Ronnie Mayberry <ronnielmayberry@gmail.com>
2016-05-20 09:10:21 -0500

    Add files via upload

---
+++ Lesson--5/High_Scores.py
@@ -0,0 +1,55 @@
+'''
+Created on November 10, 2014
+
+@author: mayberryr
+'''
+# High Scores 2.0
+# Demonstrates nested sequences.
+# Ronnie Mayberry
+# Monday, November 10, 2014
+
+scores = []
+
+choice = None
+while choice != "0":
+
+    print(
+    """
+    High Scores 2.0
+
+    0 - Quit
+    1 - List Scores
+    2 - Add a Score
+    """
+    )
+
+    choice = input("Choice: ")
+    print()
+
+    #exit
+    if choice == "0":
+        print("Good-bye.")
+
+    #display high-score table
+    elif choice == "1":
+        print("High Scores\n")
+        print("NAME\tSCORE")
+        for entry in scores:
+            score, name = entry
+            print(name, "\t", score)
+
+    #add a score
+    elif choice == "2":
+        name = input("What is the player's intials?: ")
+        score = int(input("What score did the player get? (1-100): "))
+        entry = (score, name)
+        scores.append(entry)
+        scores.sort(reverse=True)
+        scores = scores[:5] #keep only top 5 scores
+
+    #some unknown choice
+    else:
+        print("Sorry, but", choice, "isn't a valid choice.")
+
+input("\n\nPress the enter key to exit.")
+
